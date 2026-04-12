41bda3f1f95fc00fcfc5537447f62c3e8006b368
Keyrrison Vinicius de Freitas Costa <202530070934@estudante.rn.gov.br>
2026-04-12 20:03:56 -0300

    Delete backend directory

--- backend/routes/content.js
+++
@@ -1,9 +0,0 @@
-/* routes/content.js */
-import { Router } from 'express';
-import { listContent, addContent, removeContent } from '../controllers/contentController.js';
-
-const router = Router();
-router.get('/',       listContent);
-router.post('/',      addContent);
-router.delete('/:id', removeContent);
-export default router;
